f4b2ff59bdf7c34ecad55e562a58eedcfe951d27
Christoph Blumschein <christoph.blumschein@student.hpi.uni-potsdam.de>
2023-10-20 20:01:46 +0200

    Dont trace Class Initialization Simulator

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/NativeImagePointsToAnalysis.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/analysis/NativeImagePointsToAnalysis.java
@@ -123,7 +123,7 @@ public class NativeImagePointsToAnalysis extends PointsToAnalysis implements Inf
 
     @Override
     public void initializeMetaData(AnalysisType type) {
-        try (var ignored = CausalityExport.setCause(CausalityEvents.TypeReachable.create(type), CausalityExport.HeapTracing.Full)) {
+        try (var ignored = CausalityExport.setCause(CausalityEvents.TypeReachable.create(type), CausalityExport.HeapTracing.None)) {
             dynamicHubInitializer.initializeMetaData(universe.getHeapScanner(), type);
         }
     }
